c3462406c13ca6e0c2762e2b6651450735f55fab
Vladislav Kostin <70699082+ds-vs@users.noreply.github.com>
2023-02-19 23:33:04 +0300

    Чтение и десериализация данных из json-файлов.

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ConsoleApp
+﻿using System.Text.Json;
+
+namespace ConsoleApp
 {
     public class Program
     {
@@ -20,112 +22,30 @@
 
         public static Factory[] GetFactories()
         {
-            Factory[] factories = 
-            {
-                new Factory { 
-                    Id = 1, 
-                    Name = "НПЗ№1", 
-                    Description = "Первый нефтеперерабатывающий завод" 
-                },
-                new Factory { 
-                    Id = 2, 
-                    Name = "НПЗ№2", 
-                    Description = "Второй нефтеперерабатывающий завод" 
-                }
-            };
+            string fileName = "../../../Data/Factories.json";
+            string jsonString = File.ReadAllText(fileName);
+
+            var factories = JsonSerializer.Deserialize<Factory[]>(jsonString)!;
 
             return factories;
         }
 
         public static Tank[] GetTanks()
         {
-            Tank[] tanks =
-            {
-                new Tank 
-                { 
-                    Id = 1, 
-                    Name = "Резервуар 1", 
-                    Description = "Надземный - вертикальный", 
-                    Volume = 1500, 
-                    MaxVolume = 2000, 
-                    UnitId = 1
-                },
-                new Tank
-                {
-                    Id = 2,
-                    Name = "Резервуар 2",
-                    Description = "Надземный - горизонтальный",
-                    Volume = 2500,
-                    MaxVolume = 3000, 
-                    UnitId = 1
-                },
-                new Tank
-                {
-                    Id = 3,
-                    Name = "Дополнительный резервуар 24",
-                    Description = "Надземный - горизонтальный",
-                    Volume = 3000,
-                    MaxVolume = 3000,
-                    UnitId = 2
-                },
-                new Tank
-                {
-                    Id = 4,
-                    Name = "Резервуар 35",
-                    Description = "Надземный - вертикальный",
-                    Volume = 3000,
-                    MaxVolume = 3000,
-                    UnitId = 2
-                },
-                new Tank
-                {
-                    Id = 5,
-                    Name = "Резервуар 47",
-                    Description = "Подземный - двустенный",
-                    Volume = 4000,
-                    MaxVolume = 5000,
-                    UnitId = 2
-                },
-                new Tank
-                {
-                    Id = 6,
-                    Name = "Резервуар 256",
-                    Description = "Подводный",
-                    Volume = 500,
-                    MaxVolume = 500,
-                    UnitId = 3
-                }
-            };
+            string fileName = "../../../Data/Tanks.json";
+            string jsonString = File.ReadAllText(fileName);
+
+            var tanks = JsonSerializer.Deserialize<Tank[]>(jsonString)!;
 
             return tanks;
         }
 
         public static Unit[] GetUnits()
         {
-            Unit[] units = 
-            { 
-                new Unit 
-                { 
-                    Id = 1, 
-                    Name = "ГФУ-2", 
-                    Description = "Газофракционирующая установка",
-                    FactoryId = 1
-                },
-                new Unit
-                {
-                    Id = 2,
-                    Name = "АВТ-6",
-                    Description = "Атмосферно-вакуумная трубчатка",
-                    FactoryId = 1
-                },
-                new Unit
-                {
-                    Id = 3,
-                    Name = "АВТ-10",
-                    Description = "Атмосферно-вакуумная трубчатка",
-                    FactoryId = 2
-                }
-            };
+            string fileName = "../../../Data/Units.json";
+            string jsonString = File.ReadAllText(fileName);
+
+            var units = JsonSerializer.Deserialize<Unit[]>(jsonString)!;
 
             return units;
         }
