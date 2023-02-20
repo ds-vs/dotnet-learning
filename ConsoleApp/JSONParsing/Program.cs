d045dabdfe1f99d2f11318b0794dc504ab8eced4
Vladislav Kostin <70699082+ds-vs@users.noreply.github.com>
2023-02-20 20:34:26 +0300

    Решение: Урок 1. Часть 1. Парсинг JSON и применение LINQ

---
+++ ConsoleApp/JSONParsing/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace JSONParsing
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var deals = new List<Deal>();
+
+            string filePath = "../../../JSON_sample_1.json";
+
+            using (var fileReader = new StreamReader(filePath))
+            {
+                var jsonStrings = fileReader.ReadToEnd();
+
+                deals = JsonSerializer.Deserialize<List<Deal>>(jsonStrings);
+            }
+
+            var numberStrings = GetNumbersOfDeals(deals!);
+
+            Console.WriteLine($"Найдено значений: {numberStrings.Count}");
+
+            Console.Write("Идентификаторы: ");
+
+            Console.Write(string.Join(", ", numberStrings.Select(n => n)) + "\n\n");
+
+            var sumByMonth = GetSumByMonths(deals!);
+
+            foreach(var item in sumByMonth)
+            {
+                Console.WriteLine($"За {item.Month.ToString("Y")} сумма: {item.Sum}");
+            }
+        }
+
+        static IList<string> GetNumbersOfDeals(IEnumerable<Deal> deals)
+        {
+            var filteredIDs = deals
+                .Where(deal => deal.Sum >= 100)
+                .OrderBy(deal => deal.Date)
+                .Take(5)
+                .OrderBy(deal => deal.Sum)
+                .Select(deal => deal.Id)
+                .ToList();
+
+            return filteredIDs;
+        }
+
+        static IList<SumByMonth> GetSumByMonths(IEnumerable<Deal> deals)
+        {
+            var filteredSumByMonth = deals
+                .GroupBy(deal => deal.Date.ToString("Y"))
+                .Select(deal => new SumByMonth (Convert.ToDateTime(deal.Key), deal.Sum( s => s.Sum )))
+                .ToList();
+
+            return filteredSumByMonth;
+        }
+    }
+}
---
+++ ConsoleApp/JSONParsing/SumByMonth.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONParsing
+{
+    public record SumByMonth
+    {
+        public DateTime Month { get; init; }
+        public int Sum { get; init; }   
+
+        public SumByMonth(DateTime Month, int Sum) 
+        { 
+            this.Month = Month;
+            this.Sum = Sum;
+        }
+    }
+}
