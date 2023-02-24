a295d270e8bc669e4c0a78988998372aacf60961
Vladislav Kostin <70699082+ds-vs@users.noreply.github.com>
2023-02-24 12:56:01 +0300

    Получение имени организации по ИНН.

--- WebApi/externalApi/Program.cs
+++ WebApi/externalApi/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace externalApi
+﻿using Dadata;
+using Dadata.Model;
+
+namespace externalApi
 {
     public class Program
     {
-        static void Main(string[] args)
+        static readonly string token = String.Empty;
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Введите ИНН организации");
+            Console.Write(": ");
+            var inn = Console.ReadLine();
+
+            try
+            {
+                var api = new SuggestClientAsync(token);
+                var result = await api.FindParty(inn);
+
+                Console.WriteLine(result.suggestions[0].data.name.full);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
