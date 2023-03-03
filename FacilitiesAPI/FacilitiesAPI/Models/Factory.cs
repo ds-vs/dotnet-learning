c012fe4acca17290be31a163459fe3718d6656a9
Vladislav Kostin <70699082+ds-vs@users.noreply.github.com>
2023-03-03 18:17:33 +0300

    Создан новый проект на ASP.NET Core.

---
+++ FacilitiesAPI/FacilitiesAPI/Models/Factory.cs
@@ -0,0 +1,9 @@
+﻿namespace FacilitiesAPI.Models
+{
+    public class Factory
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
---
+++ FacilitiesAPI/FacilitiesAPI/Models/Unit.cs
@@ -0,0 +1,10 @@
+﻿namespace FacilitiesAPI.Models
+{
+    public class Unit
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public int FactoryId { get; set; }
+    }
+}
